dd31ba9415e274a578a6ea2cf3d3659d9917ec27
Carlos Rocha <carlos.rocha548@hotmail.com>
2025-04-24 14:58:07 -0300

    feat: created AuthService class

---
+++ app/Services/Auth/AuthService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services\Auth;
+
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+use App\Contracts\Auth\AuthContract;
+use App\Exceptions\ErrorOnAuthUserException;
+
+class AuthService implements AuthContract
+{
+    public function login(string $email, string $password): array
+    {
+        $user = User::where(User::EMAIL, $email)->first();
+
+        if (!$user || !Hash::check($password, $user->password))
+        {
+            throw new ErrorOnAuthUserException();
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return [
+            'token' => $token,
+            'user'  => $user,
+        ];
+    }
+
+    public function logout(User $user): void
+    {
+        $user->tokens()->delete();
+    }
+
+    public function me(User $user): array
+    {
+        return $user->toArray();
+    }
+}
