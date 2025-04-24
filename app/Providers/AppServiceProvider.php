dee9e1f6a367ed95382f52cc4d3e932781034c3b
Carlos Rocha <carlos.rocha548@hotmail.com>
2025-04-24 14:58:51 -0300

    feat: added Auth interface and service to binds

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,12 +9,14 @@ class AppServiceProvider extends ServiceProvider
     /** Register any application services. */
     public function register(): void
     {
-
+        $this->app->bind(
+            \App\Contracts\Auth\AuthContract::class,
+            \App\Services\Auth\AuthService::class,
+        );
     }
 
     /** Bootstrap any application services. */
     public function boot(): void
     {
-
     }
 }
