eb69ab10b27841b7cad50af21c63504520b40dc2
Carlos Rocha <carlos.rocha548@hotmail.com>
2025-04-24 21:06:53 -0300

    feat: created SellerReportMail class

---
+++ app/Mail/SellerReportMail.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SellerReportMail extends Mailable
+{
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * Report
+     *
+     * @var array<mixed>
+     */
+    public array $report;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param array<mixed> $report
+     */
+    public function __construct(array $report)
+    {
+        $this->report = $report;
+    }
+
+    /** Get the message envelope. */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Seller Report Mail',
+        );
+    }
+
+    /** Get the message content definition. */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.seller-report',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
