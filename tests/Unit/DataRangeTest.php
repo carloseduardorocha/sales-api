0ddda8c80e1a0fc06ae37462d5501331f308fdaa
Carlos Rocha <carlos.rocha548@hotmail.com>
2025-04-28 02:48:46 -0300

    feat: created DateRangeTest class

---
+++ tests/Unit/DataRangeTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit\Helpers;
+
+use App\Helpers\DateRange;
+use Carbon\Carbon;
+use Tests\TestCase;
+
+class DateRangeTest extends TestCase
+{
+    public function test_daily_returns_yesterdays_start_and_end(): void
+    {
+        Carbon::setTestNow('2024-04-27 15:00:00');
+
+        $range = DateRange::daily();
+
+        $expectedStart = Carbon::yesterday()->startOfDay();
+        $expectedEnd = Carbon::yesterday()->endOfDay();
+
+        $this->assertTrue($range['start']->equalTo($expectedStart));
+        $this->assertTrue($range['end']->equalTo($expectedEnd));
+
+        Carbon::setTestNow();
+    }
+
+    public function test_monthly_returns_last_thirty_days(): void
+    {
+        Carbon::setTestNow('2024-04-27 15:00:00');
+
+        $range = DateRange::monthly();
+
+        $expectedStart = Carbon::now()->copy()->subDays(30);
+        $expectedEnd = Carbon::now();
+
+        $this->assertTrue($range['start']->equalTo($expectedStart));
+        $this->assertTrue($range['end']->equalTo($expectedEnd));
+
+        Carbon::setTestNow();
+    }
+}
